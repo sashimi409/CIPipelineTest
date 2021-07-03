360770ba84c4a2cd793fed7acfd29a8bc5a1797e
Salim Khan <khan.salim.ali@gmail.com>
2021-07-02 22:19:16 -0400

    Added more robustness to the program, and changed naming

---
+++ Printer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+//Prints out the passed number
+//If the numvers are divisivle by 3 it will print hello instead
+//If the numver is divisible by 3 it will print world
+void PrintValue(int Num)
+{
+
+
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "Printer.h"
 
 int main() {
 
 	std::cout << "Hello! World" << std::endl;
+	PrintValue(9);
 
 	return 0;
 }
